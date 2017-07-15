79fa52bfeb7a918bd6199c4473635889bb14a339
Hudson Hughes <96hudson@gmail.com>
2017-07-14 17:26:52 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 sudo echo "" >> /home/pi/.bashrc
-sudo echo "echo Running at boot" >> sudo /home/pi/bootup.sh
+sudo echo "sudo ./home/pi/bootup.sh" >> /home/pi/.bashrc
+sudo echo "started bootup script" >> /home/pi/.bashrc
 sudo apt-get update
 sudo apt-get upgrade
 sudo reboot
