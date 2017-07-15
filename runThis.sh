9a4f675d003eaaa9e4da2d50b0c6d0825e3a57c3
Hudson Hughes <96hudson@gmail.com>
2017-07-14 17:15:53 -0700

    Update runThis.sh

--- runThis.sh
+++ runThis.sh
@@ -1,5 +1,5 @@
-cp update.sh /home/pi/
-cp bootup.sh /home/pi/
+wget --directory-prefix=/home/pi/ https://raw.githubusercontent.com/HudsonHughes/raspberrySetup/master/bootup.sh
+wget --directory-prefix=/home/pi/ https://raw.githubusercontent.com/HudsonHughes/raspberrySetup/master/update.sh
 chmod u+x /home/pi/update.sh
 chmod u+x /home/pi/bootup.sh
 ./home/pi/update.sh &
