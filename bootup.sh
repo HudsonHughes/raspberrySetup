99d1bec729fb7a2d55c4253590d910395b5be24f
Hudson Hughes <96hudson@gmail.com>
2017-07-14 18:34:59 -0700

    Update bootup.sh

--- bootup.sh
+++ bootup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-sudo apt-get install python-dev
-sudo apt-get update
-sudo apt-get install python-smbus
+sudo apt-get -y install python-dev
+sudo apt-get -y update
+sudo apt-get -y install python-smbus
 git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
 cd Adafruit_Python_GPIO
 sudo python setup.py install
 cd ..
-sudo apt-get install sense-hat
+sudo apt-get -y install sense-hat
 curl https://get.pimoroni.com/explorerhat | bash
-sudo apt-get install pimoroni
-sudo apt-get install python3_explorerhat
+sudo apt-get -y install pimoroni
+sudo apt-get -y install python3_explorerhat
 $SHELL
