8bbdbdfb5ff266027eedc02f32ef69b64117339a
Hudson Hughes <96hudson@gmail.com>
2017-07-14 18:02:15 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo echo "" >> /home/pi/.bashrc
-sudo echo "sudo lxterminal -e \"/home/pi/bootup.sh\" " >> /home/pi/.bashrc
+sudo echo "sudo @lxterminal -e \"/home/pi/bootup.sh\" " >> /home/pi/.bashrc
 sudo echo "echo started bootup script" >> /home/pi/.bashrc
 
 sudo reboot
