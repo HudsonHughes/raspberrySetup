f2f48e2561230d0317fb79b2067af30b4e55c090
Hudson Hughes <96hudson@gmail.com>
2017-07-14 18:43:24 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo echo "" >> /home/pi/.config/lxsession/LXDE-pi/autostart
-sudo echo "@lxterminal -e \"/home/pi/bootup.sh\" " >> /home/pi/.config/LXDE-pi/autostart
+sudo echo "@lxterminal -e \"/home/pi/bootup.sh\" " >> /home/pi/.config/lxsession/LXDE-pi/autostart
 sudo reboot
